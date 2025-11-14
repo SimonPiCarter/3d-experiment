8b8edaa6b870071600d7df66d0357863e93a1b11
simon <simon@DESKTOP-AL0PF2L.lan>
2025-11-14 09:31:00 +0100

    added animation for robot2

--- scenes/models/simple_robot.gd
+++ scenes/models/simple_robot.gd
@@ -1,4 +1,4 @@
-class_name SimpleRobot extends "res://scripts/ik/ik_quad_object.gd"
+class_name SimpleRobot extends IKQuadObject
 
 @onready var canon: Marker3D = $Armature/Skeleton3D/Arms/Marker3D
 
--- scenes/test/new_pipeline.gd
+++ scenes/test/new_pipeline.gd
@@ -4,16 +4,19 @@ extends Node3D
 @onready var basic_round_projectile = $MainViewport/SubViewport/Projectiles/BasicRoundProjectile
 @onready var small_bot = $MainViewport/SubViewport/SmallBot
 @onready var simple_robot = $MainViewport/SubViewport/SimpleRobot
+@onready var healer_bot: QuadEntity = $MainViewport/SubViewport/HealerBot
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_A:
-		var robot = simple_robot.get_node("SimpleRobot") as SimpleRobot
+		var robot = healer_bot.get_node("robot2") as Robot2
 		basic_round_projectile.start_pos = robot.get_source()
 		basic_round_projectile.end_pos = small_bot.global_position
 		basic_round_projectile.start()
+		robot.power()
 
 	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_E:
-		var robot = simple_robot.get_node("SimpleRobot") as SimpleRobot
+		var robot = healer_bot.get_node("robot2") as Robot2
 		basic_round_projectile_explo.start_pos = robot.get_source()
 		basic_round_projectile_explo.end_pos = small_bot.global_position
 		basic_round_projectile_explo.start()
+		robot.attack()
