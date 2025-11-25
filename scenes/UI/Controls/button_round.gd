1e5f09ae20670fd8fe7fb9ab5da7d5c417132721
simon <simon@DESKTOP-AL0PF2L.lan>
2025-11-25 23:35:00 +0100

    added better socle test

--- scenes/UI/Controls/button_round.gd
+++ scenes/UI/Controls/button_round.gd
@@ -8,16 +8,22 @@ class_name ButtonRound extends Control
 @export var hl_outline_color : Color = Color.WHITE
 @onready var button_texture: TextureRect = $TextureRect2
 @onready var shader = button_texture.material as ShaderMaterial
+@onready var button_inside: TextureRect = $MarginContainer/TextureRect3
+@onready var shader_inside = button_inside.material as ShaderMaterial
 
 func _ready() -> void:
 	shader.set_shader_parameter("outline_color", base_outline_color)
+	shader_inside.set_shader_parameter("outline_color", base_outline_color)
 
 func _process(delta: float) -> void:
 	if snap_rounding:
 		shader.set_shader_parameter("rounding", size.y/2.)
+		shader_inside.set_shader_parameter("rounding", size.y/2.-3.)
 	else:
 		shader.set_shader_parameter("rounding", rounding)
+		shader_inside.set_shader_parameter("rounding", rounding-3.)
 	shader.set_shader_parameter("outline_width", outline_width)
+	shader_inside.set_shader_parameter("outline_width", outline_width)
 
 func _on_mouse_exited() -> void:
 	shader.set_shader_parameter("outline_color", base_outline_color)
